8cd986409fe8ae507b0e13ce8e4ae24df6e4452f
TaeRimUm <horse042140@gmail.com>
2022-11-12 06:49:36 +0900

    Create character_runs.py

---
+++ Lecture05_Animation/character_runs.py
@@ -0,0 +1,21 @@
+from pico2d import *
+open_canvas()
+grass = load_image('grass.png')
+character = load_image('run_animation.png')
+
+x = 0
+frame = 0
+# 여기를 채우세요.
+while (x < 800):
+    clear_canvas()
+    grass.draw(400, 30)
+    character.clip_draw(frame * 100, 0, 100, 100, x, 90)
+    update_canvas()
+    frame = (frame + 1) % 8
+    x += 5
+    delay(0.05)
+    get_events()
+
+
+close_canvas()
+
