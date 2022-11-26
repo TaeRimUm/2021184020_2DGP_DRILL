28b782c50d28e7ec09834dc0be5ffc8f60170b0b
TaeRimUm <horse042140@gmail.com>
2022-11-26 19:32:35 +0900

    Update background.py

--- Lecture16_Scrolling/background.py
+++ Lecture16_Scrolling/background.py
@@ -6,7 +6,7 @@ from pico2d import *
 class FixedBackground:
 
     def __init__(self):
-        self.image = load_image('futsal_court.png')
+        self.image = load_image('SAYBAB_background.png')
         self.canvas_width = get_canvas_width()
         self.canvas_height = get_canvas_height()
         self.w = self.image.w
